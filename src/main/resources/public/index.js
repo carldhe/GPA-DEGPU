61f02a4f2905b810899ac316b54ca7765863adce
administrateur <carl.dhennin@etu.imt-lille-douai.fr>
2022-03-28 17:15:19 +0200

    expérience ajax

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -2,15 +2,55 @@ $(document).ready(function() {
 	let $connection = $("#connection");
 	let list;
 	
-	
 	$.getJSON("http://localhost:8080/gpa_degpu/utilisateurs",function(resp){
-		$.each(resp,function(i, field){
-			list.append(field);
+		$.each(resp,function(index){
+			
+		
 		});
 		
     });
    
+$.ajax({
+		    url: "http://localhost:8080/gpa_degpu/utilisateurs",
+		    dataType: "json",
+		    success: function(data)
+			{
+		        console.log("ça marche",data)
+				parseJSON(data)
+		    },
+			error: function(err)
+			{
+				console.log("ça plante", err)
+			}
+		});
+		
+var parseJSON = function(data)
+	{
+		$(data).each(function(i)
+		{
+			createHTML(this);
+		});
+	}
+	
+	//[i]
+	var createHTML = function (element)
+	{
+		/*for (var i= 0; i < element.length;i++)
+		{
+			var info=$("<p>").html(element.nom)
+			$('#info').append(info);
+			
+		};
+		*/
+		
+		$.each(element,function(i){
+			var info=$("<p>").html(element[i])
+			$('#info').append(info);
+		
+		});
+	}
 
+		
 	
 	
 
